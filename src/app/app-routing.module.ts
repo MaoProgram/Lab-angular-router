c13d672af33701af88f0f8671278b9c027622a7a
MauricioMahecha <124183411+MaoProgram@users.noreply.github.com>
2024-02-01 13:03:27 -0500

    laboratorio router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';  // Importa tus componentes
+import { AboutUsComponent } from './about-us/about-us.component'; 
+import { ContactComponent } from './contact/contact.component';
+import { NoexisteComponent } from './no-existe/noexiste.component';
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutUsComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'Pagina no encontrada', component: NoexisteComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+ }
